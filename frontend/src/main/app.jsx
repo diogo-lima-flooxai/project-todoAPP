958499743bfef95803886e7a048e2ad036c5501c
diogo <diogo.lima.flooxai@gmail.com>
2025-04-24 07:27:45 -0300

    feat: add routes and configuring

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Todo from '../todo/todo'
-import About from '../about/about'
 import Menu from '../template/menu'
+import Routes from './routes'
 
 require("bootstrap/dist/css/bootstrap.min.css");
 require("font-awesome/css/font-awesome.min.css");
 
 const App = (props) => (
   <div className="container">
-    <Menu/>
-    <Todo />
-    <About />
+    <Menu />
+    <Routes />
   </div>
 );
 export default App;
