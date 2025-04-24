b65a2b0ae42508e8d648d22e9b53a3d0fd056e56
diogo <diogo.lima.flooxai@gmail.com>
2025-04-24 07:45:48 -0300

    feat: add components PageHeader, TodoForm and TodoList

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+    <h2>Nossa Historia</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+    <h2>Missao e visao</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
   </div>
 )
