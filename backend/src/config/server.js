1f2e6b17755c977158003aaef569afae22c3cfcc
diogo <diogo.lima.flooxai@gmail.com>
2025-04-24 08:58:15 -0300

    fix: fixing cors middleware

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -7,6 +7,7 @@ const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true })) 
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
   console.log(`BACKEND is running on port ${port}`)
