7297803f4b0e59ee3b79f2b3ce51ba7d9c09bde1
diogo <diogo.lima.flooxai@gmail.com>
2025-04-24 07:13:11 -0300

    feat: add components About, Todo and Menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Todo from '../todo/todo'
+import About from '../about/about'
+import Menu from '../template/menu'
 
 require("bootstrap/dist/css/bootstrap.min.css");
 require("font-awesome/css/font-awesome.min.css");
 
 const App = (props) => (
   <div className="container">
-    <h1>Teste</h1>
+    <Menu/>
+    <Todo />
+    <About />
   </div>
 );
 export default App;
