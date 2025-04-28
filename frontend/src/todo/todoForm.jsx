6214f604ab59970d50e3c2b48e25a53ac99d47c2
diogo <diogo.lima.flooxai@gmail.com>
2025-04-28 07:35:38 -0300

    feat: add event to add

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from "redux";
 
 import Grid from "../template/grid";
 import IconButton from "../template/IconButton";
-import { changeDescription, search } from "./todoActions";
+import { add, changeDescription, search } from "./todoActions";
 
 class TodoForm extends Component {
   constructor(props) {
@@ -17,14 +17,16 @@ class TodoForm extends Component {
   }
 
   keyHandler(e) {
+    const { add, search, description } = this.props
     if (e.key === "Enter") {
-      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd();
+      e.shiftKey ? search() : add(description);
     } else if (e.key === "Escape") {
       props.handleClear();
     }
   }
 
   render() {
+    const { add, search, description } = this.props
     return (
       <div role="form" className="todoForm">
         <Grid cols="12 9 10">
@@ -42,12 +44,12 @@ class TodoForm extends Component {
           <IconButton
             style="primary"
             icon="plus"
-            onClick={this.props.handleAdd}
+            onClick={() => add(description)}
           ></IconButton>
           <IconButton
             style="info"
             icon="search"
-            onClick={this.props.handleSearch}
+            onClick={() => search()}
           ></IconButton>
           <IconButton
             style="default"
@@ -62,5 +64,5 @@ class TodoForm extends Component {
 
 const mapStateToProps = (state) => ({ description: state.todo.description });
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ changeDescription, search }, dispatch);
+  bindActionCreators({ add, changeDescription, search }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
